53a2a9fe05c7418f787428b3ef2200f0b4091091
Beastie PC <gskondratenko@opora.org.ua>
2017-04-27 03:47:20 +0300

    Added model mps

---
+++ app/models/mp.rb
@@ -0,0 +1,2 @@
+class Mp < ActiveRecord::Base
+end
---
+++ db/migrate/20170427004538_create_mps.rb
@@ -0,0 +1,16 @@
+class CreateMps < ActiveRecord::Migration
+  def change
+    create_table :mps do |t|
+      t.integer :deputy_id
+      t.string :first_name
+      t.string :middle_name
+      t.string :last_name
+      t.string :faction
+      t.integer :okrug
+      t.date :start_date
+      t.date :end_date
+
+      t.timestamps null: false
+    end
+  end
+end
