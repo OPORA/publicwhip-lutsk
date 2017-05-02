a4c319a07c7bb040dfc2658e93e31239cb8a1f1e
Beastie PC <gskondratenko@opora.org.ua>
2017-05-02 21:52:10 +0300

    Added redirect to show people

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @mps = Mp.order(last_name: :asc).map{|m| m.first_name + " " + m.last_name}
+    @mps = Mp.order(last_name: :asc).map{|m| m.last_name + " " + m.first_name + " " + m.middle_name}
   end
   def search_mp
-
+    mp = params[:mp].gsub(" ", "_")
+    redirect_to show_people_path(mp)
   end
 end
