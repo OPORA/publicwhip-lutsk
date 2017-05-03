419868a3fb727f8e13767865bfc86a8181b4e177
Beastie PC <gskondratenko@opora.org.ua>
2017-05-03 03:46:52 +0300

    Added list division

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -1,6 +1,17 @@
 class DivisionsController < ApplicationController
   def index
-    @divisions = Division.all
+    divisions = Division.includes(:division_info)
+    @divisions =
+        case params[:sort]
+          when "attendance"
+             divisions.order('division_infos.turnout desc', number: :desc).page params[:page]
+          when "rebellions"
+             divisions.order('division_infos.rebellions desc', number: :desc).page params[:page]
+          when "subject"
+             divisions.order(name: :asc, number: :desc).page params[:page]
+          else
+             divisions.order(date: :desc, number: :desc).page params[:page]
+        end
   end
 
   def show
--- app/helpers/divisions_helper.rb
+++ app/helpers/divisions_helper.rb
@@ -1,2 +1,13 @@
 module DivisionsHelper
+  def sort_division
+    if params[:sort] == "subject"
+      "Алфавітом"
+    elsif params[:sort] == "rebellions"
+      "Дотриманням фракційної дисципліни"
+    elsif params[:sort] == "attendance"
+      "Присутністю"
+    else
+      "Датою"
+    end
+  end
 end
