6d43a47e25a526a380364b36d859790debc5ff8d
Beastie PC <gskondratenko@opora.org.ua>
2017-05-04 16:16:02 +0300

    Added gem execjs

--- Gemfile
+++ Gemfile
@@ -18,7 +18,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
+gem 'execjs'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 gem 'jquery-ui-rails'
--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :repository, 'git@github.com:OPORA/publicwhip-lviv.git'
 set :branch, 'master'
 
 # For system-wide RVM install.
-set :rvm_path, '/usr/local/rvm/bin/rvm'
+set :rvm_path,  '/usr/local/rvm/scripts/rvm'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
