c9f27753126fb76ae47c936562d5c5cf66af8e1d
Beastie PC <gskondratenko@opora.org.ua>
2017-05-26 05:58:56 +0300

    Fix error G::UndefinedFunction: ERROR:  operator does not exist: character varying = integer

--- app/models/division.rb
+++ app/models/division.rb
@@ -10,6 +10,6 @@ class Division < ApplicationRecord
     find_by(deputy_id: id )
   end
   def self.find_by_search_query(query)
-    where("number = :query or UPPER(name) like :query_name ", {query: query.to_i, query_name: "%#{query.upcase}%" })
+    where("UPPER(name) like :query_name ", { query_name: "%#{query.upcase}%" })
   end
 end
