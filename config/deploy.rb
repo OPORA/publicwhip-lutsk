cef673185abf9ff27785f008fde286ac1405fbdd
Beastie PC <gskondratenko@opora.org.ua>
2017-05-26 05:51:09 +0300

    Change ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :user, 'root'          # Username in the server to SSH to.
 # shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 set :shared_dirs, fetch(:shared_dirs, []).push('log', 'public/image')
 set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
-invoke :'rvm:use', 'ruby-2.2.5@default'
+invoke :'rvm:use', 'ruby-2.4.1@default'
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
 task :environment do
