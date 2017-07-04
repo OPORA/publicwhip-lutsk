ab6ead79c1469ccbb841f3e6c37d85a00491ed7e
Beastie PC <gskondratenko@opora.org.ua>
2017-07-05 02:36:46 +0300

    Added home page designer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
     @mps = Mp.order(last_name: :asc).map{|m| m.last_name + " " + m.first_name + " " + m.middle_name}
+    @last_division = Division.includes(:division_info).last(4)
+    @top_mp_rebellions = Mp.includes(:mp_info).where(end_date: nil).to_a.sort_by{ |m| [-(m.mp_info.attendance_fraction || -1), m.last_name, m.first_name, m.middle_name, m.faction, m.okrug ]}.reverse[0 .. 3]
   end
   def search_mp
     mp = params[:mp].gsub(" ", "_")
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def body_class
       controller.controller_path
   end
+  def last_sessions_rada
+    Division.pluck(:date).last.strftime('%d.%m.%Y')
+  end
+  def last_update_rada
+    Division.pluck(:updated_at).last.strftime('%d.%m.%Y / %H:%M')
+  end
 end
