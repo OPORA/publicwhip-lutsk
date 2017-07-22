27b3e4ca766d1e6c16c6c3a349f6585066561d12
Beastie PC <gskondratenko@opora.org.ua>
2017-07-22 20:18:47 +0300

    Added filters for sumisne voted

--- app/controllers/sumisne_holosuvannia_controller.rb
+++ app/controllers/sumisne_holosuvannia_controller.rb
@@ -1,9 +1,20 @@
 class SumisneHolosuvanniaController < ApplicationController
   def init
-
+     if params[:month].nil? or params[:month].blank?
+       params[:month] = "full"
+     end
+    if params[:party].nil? or params[:party].blank?
+      params[:party] = "Блок Петра Порошенка"
+    end
+     @party = VoteFaction.pluck(:faction).uniq
+     @month = VoteFaction.pluck(:date).uniq
   end
   def api
-    @vote = VoteFaction.order(vote_aye: :desc).map{|v| {faction: v.faction, date: v.date.strftime('%Y-%m'), vote_aye: v.vote_aye}}
+    if params[:month] == "full"
+       @vote = VoteFaction.order(vote_aye: :desc).map{|v| {faction: v.faction, date: v.date.strftime('%Y-%m'), vote_aye: v.vote_aye}}
+    else
+       @vote = VoteFaction.where(date: Date.strptime(params[:month], '%Y-%m')).order(vote_aye: :desc).map{|v| {faction: v.faction, date: v.date.strftime('%Y-%m'), vote_aye: v.vote_aye}}
+    end
     render :json => @vote
   end
 end
