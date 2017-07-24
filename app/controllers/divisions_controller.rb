bd4145ab5511ee57590660aac85b88aa1977a9af
Beastie PC <gskondratenko@opora.org.ua>
2017-07-24 08:17:01 +0300

    Added filter voted  and search voted

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -30,15 +30,17 @@ class DivisionsController < ApplicationController
             if params[:filter_max].nil?
               params[:filter_max] = 90
             end
-            divisions.order('division_infos.turnout desc', number: :desc).page(params[:page]).per(params[:per])
+            divi = divisions.to_a.find_all{|m| m.division_info.attendance_division.to_f >= params[:filter_min].to_f/100 and m.division_info.attendance_division.to_f <= params[:filter_max].to_f/100 }.sort_by{ |m| [-(m.division_info.attendance_division || -1), m.date, m.number ]}
+            Kaminari.paginate_array(divi).page(params[:page]).per(params[:per])
           when "rebellions"
             if params[:filter_min].nil?
-              params[:filter_min] = 30
+              params[:filter_min] = 20
             end
             if params[:filter_max].nil?
               params[:filter_max] = 90
             end
-            divisions.order('division_infos.rebellions desc', number: :desc).page(params[:page]).per(params[:per])
+            divi =divisions.to_a.find_all{|m| m.division_info.rebellions_fraction.to_f >= params[:filter_min].to_f/100 and m.division_info.rebellions_fraction.to_f <= params[:filter_max].to_f/100 }.sort_by{ |m| [-(m.division_info.rebellions_fraction || -1), m.date, m.number ]}
+            Kaminari.paginate_array(divi).page(params[:page]).per(params[:per])
           else
             divisions.order(date: :desc, number: :desc).page(params[:page]).per(params[:per])
         end
--- app/models/division_info.rb
+++ app/models/division_info.rb
@@ -3,4 +3,7 @@ class DivisionInfo < ApplicationRecord
   def attendance_division
     turnout.to_f / possible_turnout if possible_turnout > 0
   end
+  def rebellions_fraction
+    rebellions.to_f / turnout if turnout > 0
+  end
 end
