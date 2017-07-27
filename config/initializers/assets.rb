77c8911987e72720b7052d873838ff6b6d3e9b6a
Beastie PC <gskondratenko@opora.org.ua>
2017-07-27 17:08:18 +0300

    Added link for save png deputy and get csv file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( about.scss  bootstrap.css  bootstrap.css.map  divisions.scss  errors.scss  help.scss  home.scss  people.scss  style.css  sumisne_holosuvannia.scss  ukr_datepiker.js)
+Rails.application.config.assets.precompile += %w( about.scss  bootstrap.css  bootstrap.css.map  divisions.scss  errors.scss  help.scss  home.scss  people.scss  style.css  sumisne_holosuvannia.scss  ukr_datepiker.js html2canvas.js)
