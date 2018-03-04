d3da3474894a974c2138bed38bae758177c3d927
Beastie PC <gskondratenko@opora.org.ua>
2018-03-05 01:40:11 +0200

    Fix rebellions_fraction if nil rebeloions

--- app/models/mp_info.rb
+++ app/models/mp_info.rb
@@ -1,7 +1,11 @@
 class MpInfo < ApplicationRecord
   belongs_to :mp, primary_key: :deputy_id, foreign_key: :deputy_id
   def rebellions_fraction
-    rebellions.to_f / votes_attended if votes_attended > 0
+    unless rebellions
+      rebellions.to_f / votes_attended if votes_attended > 0
+    else
+     return nil
+    end
   end
   def attendance_fraction
     votes_attended.to_f / votes_possible if votes_possible > 0
