09d4b3cf76b881614c37a84dff98ca54b8f1007a
Beastie PC <gskondratenko@opora.org.ua>
2018-07-10 02:41:36 +0300

    Addede history and radio button style css in division add polisy support

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -1,6 +1,7 @@
 class Policy < ApplicationRecord
   has_many :policy_divisions
   has_many :policy_person_distances
+  has_paper_trail
   def self.find_by_search_query(query)
     where("UPPER(name) like :query_name ", { query_name: "%#{query.upcase}%" })
   end
--- app/models/policy_division.rb
+++ app/models/policy_division.rb
@@ -1,5 +1,52 @@
 class PolicyDivision < ApplicationRecord
   belongs_to :policy
   belongs_to :division
-  validates :division_id, presence: true, uniqueness: true
+  has_paper_trail
+  validates :division_id, presence: true, uniqueness:  { scope: :policy_id}
+  after_create :create_person_distanse
+  def create_person_distanse
+    not_agre = ["absent","abstain", "not_voted"]
+    if self.support == "aye_strong"
+      possible = 50
+      support = "aye"
+      assume_agre = 50
+      assume_against = 0
+      assume_no_agre = 25
+    elsif self.support = "against_strong"
+      possible = 50
+      support = "against"
+      assume_agre = 50
+      assume_against = 0
+      assume_no_agre = 25
+    else
+      possible = 10
+      support = self.support
+      assume_agre = 10
+      assume_against = 0
+      assume_no_agre = 5
+    end
+    self.division.votes.each do |v|
+      p v.vote
+      policy_persone_distanse = PolicyPersonDistance.where(deputy_id: v.mp.deputy_id, policy_id: self.policy_id).first_or_create
+      if support == v.vote
+         assume = assume_agre
+         policy_persone_distanse.assume = policy_persone_distanse.assume + assume
+         policy_persone_distanse.possible = policy_persone_distanse.possible + possible
+         policy_persone_distanse.support = (policy_persone_distanse.assume.to_f/policy_persone_distanse.possible) * 100
+         policy_persone_distanse.save
+      elsif not_agre.include?(v.vote)
+         assume = assume_no_agre
+         policy_persone_distanse.assume = policy_persone_distanse.assume + assume
+         policy_persone_distanse.possible = policy_persone_distanse.possible + possible
+         policy_persone_distanse.support = (policy_persone_distanse.assume.to_f/policy_persone_distanse.possible) * 100
+         policy_persone_distanse.save
+      else
+         assume = assume_against
+         policy_persone_distanse.assume = policy_persone_distanse.assume + assume
+         policy_persone_distanse.possible = policy_persone_distanse.possible + possible
+         policy_persone_distanse.support = (policy_persone_distanse.assume.to_f/policy_persone_distanse.possible) * 100
+         policy_persone_distanse.save
+      end
+    end
+  end
 end
