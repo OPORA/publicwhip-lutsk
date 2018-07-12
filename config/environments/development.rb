f58a54f437585c18640699fbcc6424df82f931b5
Beastie PC <gskondratenko@opora.org.ua>
2018-07-12 22:28:51 +0300

    Config email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,4 +52,16 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: 'mail.oporaua.org',
+      port: 2525,
+      domain: 'oporaua.org',
+      authentication: :plain,
+      user_name: Settings.email,
+      password: Settings.email_password,
+      enable_starttls_auto: true
+  }
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: Settings.project_host , protocol: 'https' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: 'mail.oporaua.org',
+      port: 2525,
+      domain: 'oporaua.org',
+      authentication: :plain,
+      user_name: Settings.email,
+      password: Settings.email_password,
+      enable_starttls_auto: true
+  }
 end
