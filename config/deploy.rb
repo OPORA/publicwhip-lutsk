9ad01ced0e67d2d4b47daad2a0213314003dbfc4
Beastie PC <gskondratenko@opora.org.ua>
2018-07-12 22:44:41 +0300

    Config mina added copy settings.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :port, '1122'           # SSH port number.
 
 # shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 set :shared_dirs, fetch(:shared_dirs, []).push('log', 'public/image')
-set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml', 'config/settings.yml')
 invoke :'rvm:use', 'ruby-2.4.1@default'
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
