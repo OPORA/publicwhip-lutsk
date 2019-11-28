8e5ea97230f5d26d279ff92fb53c6713ca58834f
Beastie PC <gskondratenko@opora.org.ua>
2019-11-28 16:36:44 +0200

    Fix show party at sumisne voted, when party were went out

--- app/controllers/sumisne_holosuvannia_controller.rb
+++ app/controllers/sumisne_holosuvannia_controller.rb
@@ -1,7 +1,7 @@
 class SumisneHolosuvanniaController < ApplicationController
   def init
     unless params[:party].nil? or params[:party].blank?
-      part_ser = PartyFriend.where(party: params[:party])
+      part_ser = PartyFriend.joins(:mps,:mpss).where(party: params[:party])
       if part_ser.blank?
         redirect_to sumisne_holosuvannia_path, :notice => "Нажаль партії не було знайдено, спробуйте виконати інший пошуковий запит"
       end
@@ -10,18 +10,19 @@ class SumisneHolosuvanniaController < ApplicationController
        params[:month] = "full"
      end
     if params[:party].nil? or params[:party].blank?
-      params[:party] =  PartyFriend.order("RANDOM()").pluck(:party).first
+      params[:party] =  PartyFriend.joins(:mps,  :mpss).order("RANDOM()").pluck(:party).first
       params[:example_party] = 1
     end
-     @party = PartyFriend.pluck(:party).uniq
-     @month = PartyFriend.order(date_party_friend: :desc).pluck(:date_party_friend).uniq
+     @party = PartyFriend.joins(:mps, :mpss).pluck(:party).uniq
+     @month = PartyFriend.joins(:mps,:mpss).order(date_party_friend: :desc).pluck(:date_party_friend).uniq
   end
   def api
-    party = PartyFriend.pluck(:party).uniq
+    party = PartyFriend.joins(:mps, :mpss).pluck(:party).uniq
     if params[:month] == "full"
-      vote =  Division.all.size
+       vote =  Division.all.size
        @vote =  [{faction: params[:party], date: "full", vote_aye: vote}]
-       @vote += PartyFriend.where(party: params[:party]).order(count: :desc).map{|v| {faction: v.friend_party, date: v.date_party_friend.strftime('%Y-%m'), vote_aye: v.count}}
+       @vote += PartyFriend.joins(:mpss).where(party: params[:party]).order(count: :desc).map{|v| {faction: v.friend_party, date: v.date_party_friend.strftime('%Y-%m'), vote_aye: v.count}}
+       p @vote
     else
       date = Date.strptime(params[:month],'%Y-%m')
       year_month = date.strftime('%Y-%m')
@@ -29,7 +30,7 @@ class SumisneHolosuvanniaController < ApplicationController
       date_max = date + 1.month
       vote = Division.where("date >= ? AND date < ?", date_min, date_max).size
       @vote =  [{faction: params[:party], date: params[:month], vote_aye: vote}]
-      @vote += PartyFriend.where(party: params[:party], date_party_friend: Date.strptime(params[:month], '%Y-%m')).order(count: :desc).map{|v| {faction: v.friend_party, date: v.date_party_friend.strftime('%Y-%m'), vote_aye: v.count}}
+      @vote += PartyFriend.joins(:mpss).where(party: params[:party], date_party_friend: Date.strptime(params[:month], '%Y-%m')).order(count: :desc).map{|v| {faction: v.friend_party, date: v.date_party_friend.strftime('%Y-%m'), vote_aye: v.count}}
     end
     vote_party = @vote.map{|p| p[:faction]}
     party_not_voted = party.find_all{|v| vote_party.include?(v) == false }
--- app/models/party_friend.rb
+++ app/models/party_friend.rb
@@ -1,2 +1,4 @@
 class PartyFriend < ApplicationRecord
+    has_many :mps,->{where(mps:{end_date:"9999-12-31"})}, class_name: 'Mp', foreign_key: "faction", primary_key: "party"
+    has_many :mpss, -> {where(mps:{end_date:"9999-12-31"})},  class_name: "Mp", foreign_key: "faction", primary_key: "friend_party"
 end
